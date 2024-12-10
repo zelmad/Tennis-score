582f1fe78e3b01e8b8d0be1152688cd9bc84ba9b
zelmadmohamed1994@gmail.com <Lhrto1994@>
2024-12-10 22:54:59 +0200

    Initiate winner tennis round project

---
+++ src/main/java/org/zelmad/Tennis.java
@@ -0,0 +1,8 @@
+package org.zelmad;
+
+public class Tennis {
+
+    public String getWinner(String gameResults){
+        return null;
+    }
+}
---
+++ src/test/java/TennisTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class TennisTest {
+
+    @Test
+    public void test_player_a_win() {
+
+    }
+
+    @Test
+    public void test_player_b_win() {
+
+    }
+
+    @Test
+    public void test_player_a_b_deuce_a_win() {
+
+    }
+
+    @Test
+    public void test_player_a_b_deuce_b_win() {
+
+    }
+}
