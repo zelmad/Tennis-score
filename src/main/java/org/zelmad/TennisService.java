1bc33edef34c54516a4287fc333f2bb18c267ffc
zelmadmohamed1994@gmail.com <Lhrto1994@>
2024-12-11 00:33:28 +0200

    Implement method logic

--- src/main/java/org/zelmad/TennisService.java
+++ src/main/java/org/zelmad/TennisService.java
@@ -1,8 +1,52 @@
 package org.zelmad;
 
-public class Tennis {
+import java.util.HashMap;
+import java.util.Map;
 
-    public String getWinner(String gameResults){
-        return null;
+public class TennisService {
+
+    public static final String ADVANTAGE = "advantage";
+
+    public String getWinner(String input){
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "0");
+        map.put("B", "0");
+        for(int i=0; i<input.length();i ++){
+            String playerAScore = map.get("A");
+            String playerBScore = map.get("B");
+            if('A' == input.charAt(i)) {
+                if(ADVANTAGE.equals(playerBScore)){
+                    map.put("B", "40");
+                } else if(ADVANTAGE.equals(playerAScore) || ("40".equals(playerAScore) && !"40".equals(playerBScore))) {
+                    return "Player A wins the game";
+                } else  {
+                    addValue("A", map);
+                }
+                System.out.println("Player A : " + map.get("A") + " / Player B : " + map.get("B"));
+            } else {
+                if(ADVANTAGE.equals(playerAScore)){
+                    map.put("A", "40");
+                } else if(ADVANTAGE.equals(playerBScore) || (!"40".equals(playerAScore) && "40".equals(playerBScore))) {
+                    return "Player B wins the game";
+                } else  {
+                    addValue("B", map);
+                }
+                System.out.println("Player A : " + map.get("A") + " / Player B : " + map.get("B"));
+            }
+        }
+        return "";
+    }
+
+    private void addValue(String key, Map<String, String> map) {
+        String value = map.get(key);
+        if ("0".equals(value)){
+            map.put(key, "15");
+        }else if("15".equals(value)){
+            map.put(key, "30");
+        } else if("30".equals(value)) {
+            map.put(key, "40");
+        } else if("40".equals(value)) {
+            map.put(key, ADVANTAGE);
+        }
     }
 }
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,24 +1,43 @@
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zelmad.TennisService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TennisTest {
 
+    public TennisService tennisService;
+
+    @BeforeEach
+    void setUp(){
+        tennisService = new TennisService();
+    }
+
     @Test
     public void test_player_a_win() {
-
+        String winnerPlayer = tennisService.getWinner("ABABAA");
+        String expectedWinner = "Player A wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
     }
 
     @Test
     public void test_player_b_win() {
-
+        String winnerPlayer = tennisService.getWinner("BABABB");
+        String expectedWinner = "Player B wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
     }
 
     @Test
     public void test_player_a_b_deuce_a_win() {
-
+        String winnerPlayer = tennisService.getWinner("ABABABAA");
+        String expectedWinner = "Player A wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
     }
 
     @Test
     public void test_player_a_b_deuce_b_win() {
-
+        String winnerPlayer = tennisService.getWinner("ABABABBABB");
+        String expectedWinner = "Player B wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
     }
 }
